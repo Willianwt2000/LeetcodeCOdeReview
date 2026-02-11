5d13cb72a96da0a36f145cf8ca8a39ee446677d6
Willianwt2000 <willianmetelien@gmail.com>
2026-02-11 17:40:37 -0400

    fix

--- minilibrary/views.py
+++ minilibrary/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    try:
+        author_id = request.GET.get('author')
+        return render(request, 'minilibrary/minilibrary.html', 
+                {
+                    'text': 'Bienvenido a la Mini Biblioteca', 'name': 'Usuario', 'author': author_id
+                }
+            )
+    except Exception as e:
+        return HttpResponse(f"Pagina no encontrada: {str(e)}", status=404)
